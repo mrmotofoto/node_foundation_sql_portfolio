1af88a270620f0d90534c9b11d5aebb8683d0198
Richard Stiehm <richardstiehm@gmail.com>
2016-04-23 10:11:29 -0400

    Db Test

--- routes/admin.js
+++ routes/admin.js
@@ -7,7 +7,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
 	host: 'localhost',
 	user: 'root',
-	password: "harry",
+	password: "XXXX",
 	database: 'portfoilio'
 
 });
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: "harry",
+    password: "xxxx",
     database: 'portfoilio'
 
 });
